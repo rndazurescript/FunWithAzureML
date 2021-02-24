74ecb0fc0ce9736156513d0bf4a586ecb62fe072
rndazurescript <rndazurescript@users.noreply.github.com>
2021-02-24 13:01:30 +0000

    Added script to install common libs

---
+++ CommonPythonPackages.sh
@@ -0,0 +1,6 @@
+# Linting code
+pip install flake8
+# Code formater
+# For black, you may need to upgrade or remove enum34
+# https://stackoverflow.com/questions/43124775/why-python-3-6-1-throws-attributeerror-module-enum-has-no-attribute-intflag
+pip install black
