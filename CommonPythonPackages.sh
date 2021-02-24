e17607edce291e87de4b8be5032a937a8220dbbc
rndazurescript <rndazurescript@users.noreply.github.com>
2021-02-24 13:04:40 +0000

    Upgrade existing libs

--- CommonPythonPackages.sh
+++ CommonPythonPackages.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
+# Upgrade azureml-sdk
+pip install --upgrade azureml-sdk
 # Linting code
-pip install flake8
+pip install --upgrade flake8
 # Code formater
 # For black, you may need to upgrade or remove enum34
 # https://stackoverflow.com/questions/43124775/why-python-3-6-1-throws-attributeerror-module-enum-has-no-attribute-intflag
-pip install black
+pip install --upgrade black
