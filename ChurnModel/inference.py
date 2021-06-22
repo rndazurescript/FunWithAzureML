e9258cb7b12be1f428bd502c28ecf3cbd5b91d9c
rndazurescript <rndazurescript@users.noreply.github.com>
2021-06-22 12:08:31 +0000

    Formated inferences

--- ChurnModel/inference.py
+++ ChurnModel/inference.py
@@ -16,6 +16,7 @@ class ChurnModel(object):
         model_directory = "./model"  # Load local if not attached to AzureML
         from azureml.core.model import Model
         from azureml.exceptions import ModelNotFoundException
+
         try:
             model_directory = Model.get_model_path(model_name, version=version)
         except ModelNotFoundException as e:
@@ -33,7 +34,7 @@ class ChurnModel(object):
 
 # The following code will run if we invoke this file directly from
 # command line, e.g. python inference.py
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Set log level to debug
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
@@ -45,21 +46,28 @@ if __name__ == '__main__':
     logger.addHandler(handler)
     # Initialize model
     model = ChurnModel()
-    test_data = pd.DataFrame({
-        'id': ['loyal_user', 'churning_user'],
-        'customer_tenure': [32, 8],
-        'product_tenure': [2, 8],
-        'activity_last_6_months': [60, 12],
-        'activity_last_12_months': [120, 14]
-    }, columns=['id', 'customer_tenure', 'product_tenure',
-                'activity_last_6_months', 'activity_last_12_months'
-                ]
+    test_data = pd.DataFrame(
+        {
+            "id": ["loyal_user", "churning_user"],
+            "customer_tenure": [32, 8],
+            "product_tenure": [2, 8],
+            "activity_last_6_months": [60, 12],
+            "activity_last_12_months": [120, 14],
+        },
+        columns=[
+            "id",
+            "customer_tenure",
+            "product_tenure",
+            "activity_last_6_months",
+            "activity_last_12_months",
+        ],
     )
     print("Test dataset")
     print(test_data)
     result = model.run(test_data)
     # Add the user id to the resulted probability
-    output = pd.DataFrame(zip(test_data['id'].values, result),
-                          columns=['id', 'churn_probability'])
+    output = pd.DataFrame(
+        zip(test_data["id"].values, result), columns=["id", "churn_probability"]
+    )
     print("Results")
     print(output)
